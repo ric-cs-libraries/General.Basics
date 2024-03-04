bf94f03b2f3cfa98a642d039460d79f35b9576d6
Richard PAYET <rick.info.rsoc@gmail.com>
2024-03-04 17:24:14 +0100

    General.Basics v1.8 : Ajout de Check.NotNull(...)

---
+++ _SRC/General.Basics/Checks/Check.cs
@@ -0,0 +1,10 @@
+﻿namespace General.Basics.Checks;
+
+public static class Check
+{
+    public const string NOT_NULL_MESSAGE_FORMAT = "The variable '{0}' is null but must not !";
+    public static void NotNull(object x, string variableName)
+    {
+        _ = x ?? throw new NullReferenceException( string.Format(NOT_NULL_MESSAGE_FORMAT, variableName) );
+    }
+}
