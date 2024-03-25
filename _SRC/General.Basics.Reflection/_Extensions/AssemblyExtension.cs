0abc0c071fa2ceb6e059193aa45c640409dd7c4d
Richard PAYET <rick.info.rsoc@gmail.com>
2024-03-25 23:29:04 +0100

    General.Basics.Reflection v2.1 : AssemblyExtension : ajout de GetInstancesFromClassesImplementing_ et GetInstancesFromGenericClassesImplementing_.

--- _SRC/General.Basics.Reflection/_Extensions/AssemblyExtension.cs
+++ _SRC/General.Basics.Reflection/_Extensions/AssemblyExtension.cs
@@ -1,10 +1,11 @@
-﻿using System.Linq;
-using System.Reflection;
-
+﻿using System.Reflection;
 using General.Basics.Reflection.POO;
+using General.Basics.Reflection.DynamicCalls;
+using General.Basics.Reflection.DynamicCalls.Interfaces;
+
+
 
 namespace General.Basics.Reflection.Extensions;
-
 
 public static class AssemblyExtension
 {
@@ -32,6 +33,47 @@ public static class AssemblyExtension
         IEnumerable<Class> classes = types.Select(type => (type.IsAbstract) ? new AbstractClass(type) : new Class(type));
 
         return classes.ToList();
+    }
+
+    public static IReadOnlyList<TInterface> GetInstancesFromClassesImplementing_<TInterface>(this Assembly assembly, object[]? constructorParams = null)
+    {
+        Type typeOfInterface = typeof(TInterface);
+        Interface @interface = new(typeOfInterface);
+
+        IInstanciator instanciator = Instanciator.Create();
+
+        IEnumerable<Class> classes = assembly.GetConcreteClassesImplementingTheInterface_(@interface)
+                                             .Where((Class concreteClass) => !concreteClass.Type.IsGenericType)
+                                             ;
+
+        IEnumerable<TInterface> instances = classes
+                                           .Select((Class concreteClass) => instanciator.GetInstance(concreteClass, constructorParams))
+                                           .Cast<TInterface>();
+
+        return instances.ToList();
+    }
+
+    public static IReadOnlyList<TInterface> GetInstancesFromGenericClassesImplementing_<TInterface>(this Assembly assembly, Type[] genericParametersType, object[]? constructorParams = null)
+    {
+        Type typeOfInterface = typeof(TInterface);
+        Interface @interface = new(typeOfInterface);
+
+        IInstanciatorForGeneric instanciatorForGeneric = InstanciatorForGeneric.Create();
+
+        int nbGenericParameters = genericParametersType.Length;
+
+        IEnumerable<Class> classes = assembly.GetConcreteClassesImplementingTheInterface_(@interface)
+                                            .Where((Class concreteClass) => {
+                                                bool b = concreteClass.Type.IsGenericType && concreteClass.Type.GetGenericArguments().Length == nbGenericParameters;
+                                                return b;
+                                             })
+                                            ;
+
+        IEnumerable<TInterface> instances = classes
+                                           .Select((Class concreteGenericClass) => instanciatorForGeneric.GetInstance(concreteGenericClass, genericParametersType, constructorParams))
+                                           .Cast<TInterface>();
+
+        return instances.ToList();
     }
     #endregion Reflection
 }
