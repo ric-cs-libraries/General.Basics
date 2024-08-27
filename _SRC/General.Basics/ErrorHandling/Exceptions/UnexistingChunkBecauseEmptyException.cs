f050add374d18ea3d459586aec965692289671b8
Richard PAYET <rick.info.rsoc@gmail.com>
2024-08-27 02:48:00 +0200

    UnexistingChunkBecauseEmptyException : le param. startIndex peut désormais être null, idem pour endIndex.

--- _SRC/General.Basics/ErrorHandling/Exceptions/UnexistingChunkBecauseEmptyException.cs
+++ _SRC/General.Basics/ErrorHandling/Exceptions/UnexistingChunkBecauseEmptyException.cs
@@ -5,7 +5,7 @@ public class UnexistingChunkBecauseEmptyException : Exception
     public const string MESSAGE_FORMAT = "In {0}, Unexisting Chunk [startIndex='{1}'; endIndex='{2}'] ; because it is Empty !";
     public override string Message { get; }
 
-    public UnexistingChunkBecauseEmptyException(int startIndex, int endIndex, string subject)
+    public UnexistingChunkBecauseEmptyException(int? startIndex, int? endIndex, string subject)
     {
         Message = string.Format(MESSAGE_FORMAT, subject, startIndex, endIndex);
     }
