47181a3f1ffea9e43d431a3cf90f78181b2b94c9
Richard PAYET <rick.info.rsoc@gmail.com>
2025-01-13 22:48:59 +0100

    General.Basics.ReflectionExtended.POO.TypeExtension : ajout d'un commentaire, sur méthode IsOfType_().

--- _SRC/General.Basics.ReflectionExtended/POO/TypeExtension.cs
+++ _SRC/General.Basics.ReflectionExtended/POO/TypeExtension.cs
@@ -5,11 +5,13 @@ namespace General.Basics.ReflectionExtended.POO;
 
 public static partial class TypeExtension
 {
-    public static bool IsOfType_(this Type type, Type ofType) //ATTENTION: ne prend pas en compte l'héritage ou l'implém. d'interfaces.
+    //ATTENTION: IsOfType_ ne prend pas en compte l'héritage, ou l'implém. d'interfaces.
+    //           Utiliser type.BaseType au lieu de type, peut alors éventuellement être une solution.
+    public static bool IsOfType_(this Type type, Type ofType)
     {
         //Utile pour tester ce genre de chose (avec type = thisType, et ofType = typeof(MaClasse<>)) :
         //                    typeof(MyClasse<>).IsAssignableFrom(thisType)   qui renvoie malheureusement toujours false <<<<<
-        // C-à-d qu'ici on veut renvoyer true SI thisType est EXACTEMENT de type MyClasse<T,...> QUEL QUE SOIT  T,...  en fait !!
+        // C-à-d qu'ici on veut renvoyer true SI thisType est >EXACTEMENT< LE type MyClasse<T,...> QUEL QUE SOIT  T,...  en fait !!
 
         bool response = type.FullName is not null && ofType.FullName is not null
                         && type.FullName!.StartsWith(ofType.FullName!);
