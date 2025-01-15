a44702c93e3e078903d0909c8304bedbb7738419
Richard PAYET <rick.info.rsoc@gmail.com>
2025-01-15 14:42:54 +0100

    .Basics  v4.3.0 : DateTimeOffsetExtension :                 - chgt nom namespace -&gt; General.Basics.Extensions.Dates.Static 				        - DateTimeOffsetExtension.GetCurrentTimeStamp_() -&gt; en SECONDS et non plus milliseconds !! 			          - nouvelle : DateTimeOffsetExtension.GetCurrentMsTimeStamp_() -&gt; en milliseconds ! 				        - Pas d'incrém. numéro version majeure (pas 5), car maîtrisé... par ailleurs.

---
+++ _SRC/General.Basics/_Extensions/Dates/Static/DateTimeOffsetExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace General.Basics.Extensions.Dates.Static;
+
+[ExcludeFromCodeCoverage]
+public static class DateTimeOffsetExtension
+{
+    /// <summary>
+    ///     Timestamp in MILLISECONDS.
+    /// </summary>
+    public static long GetCurrentMsTimeStamp_()
+    {
+        return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+    }
+
+    /// <summary>
+    ///     Timestamp in SECONDS.
+    /// </summary>
+    public static long GetCurrentTimeStamp_()
+    {
+        return GetCurrentMsTimeStamp_() / 1000;
+    }
+
+}
