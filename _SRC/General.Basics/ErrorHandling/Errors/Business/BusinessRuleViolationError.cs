1259e34d4f783033af478cc09151cc3cbb883d88
Richard PAYET <rick.info.rsoc@gmail.com>
2025-01-26 23:42:57 +0100

    .Basics : ErrorHandling : désormais BusinessRuleViolationError hérite de  ErrorWithOptionalCode. Et plus exactement : pas de notion de Code pour BusinessRuleViolationError, car le type en héritant à lui seul caractérisera la nature de l'erreur.

--- _SRC/General.Basics/ErrorHandling/Errors/Business/BusinessRuleViolationError.cs
+++ _SRC/General.Basics/ErrorHandling/Errors/Business/BusinessRuleViolationError.cs
@@ -1,14 +1,9 @@
 ﻿namespace General.Basics.ErrorHandling;
 
-public record BusinessRuleViolationError : Error
+public record BusinessRuleViolationError : ErrorWithOptionalCode
 {
-    public BusinessRuleViolationError(string code, string debugMessageTemplate = "Business Rule Violation", IEnumerable<string?>? placeholderValues = null) 
-        : base(code, debugMessageTemplate, placeholderValues)
-    {
-    }
-
-    public BusinessRuleViolationError(string code, string dataLabel, string? dataInvalidValue, string debugMessageTemplate = "'{0}' is not a valid {1} value.")
-        : this(code, debugMessageTemplate, new[] { dataInvalidValue, dataLabel })
+    public BusinessRuleViolationError(string debugMessageTemplate = "", IEnumerable<string?>? placeholderValues = null)
+        : base(debugMessageTemplate, placeholderValues)
     {
     }
 }
